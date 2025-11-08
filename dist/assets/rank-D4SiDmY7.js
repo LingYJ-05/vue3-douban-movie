8a3816a178e61b8059902f607f8ac79caaab70d4
Lyj <3218406937@qq.com>
2025-11-08 13:37:32 +0800

    fix: 修复构建问题以支持Vercel部署

---
+++ dist/assets/rank-D4SiDmY7.js
@@ -0,0 +1,1 @@
+import{d as t}from"./index-DvxNNp22.js";const e=t("rank",{state:()=>({showCards:!0}),actions:{updateShowCards(s){this.showCards=s}},getters:{getShowCards(){return this.showCards}}});export{e as u};
